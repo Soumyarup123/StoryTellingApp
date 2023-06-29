feec68f6dfed02a720d636546a256480d574f66e
Soumyarup123 <84226112+Soumyarup123@users.noreply.github.com>
2023-06-29 12:26:28 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB8SLc6Tma5uOrcmJIM7Hf8iGFD1hDLOVM",
+    authDomain: "storytellingapp-6eccb.firebaseapp.com",
+    databaseURL: "https://storytellingapp-6eccb-default-rtdb.firebaseio.com",
+    projectId: "storytellingapp-6eccb",
+    storageBucket: "storytellingapp-6eccb.appspot.com",
+    messagingSenderId: "1015209732450",
+    appId: "1:1015209732450:web:9211cef1164a9b7aaba43b"
+  };
